4b38f55c62be39970e74eca7e7660134099f374f
lWeRl <werasdas@gmail.com>
2016-11-16 15:48:35 +0300

    counter convergence method add

--- src/lesson5/HomeWork5.java
+++ src/lesson5/HomeWork5.java
@@ -8,6 +8,7 @@ public class HomeWork5 {
     static float[] arr2 = new float[size];
     static float[] arr3 = new float[size];
     static float[] arr4 = new float[size];
+    static float[] arr5 = new float[size];
 
     static long method1() {
         for (int i = 0; i < size; i++) {
@@ -132,6 +133,42 @@ public class HomeWork5 {
         return System.currentTimeMillis() - a;
     }
 
+    static long method5() {
+        for (int i = 0; i < size; i++) {
+            arr5[i] = 1.0f;
+        }
+        long a = System.currentTimeMillis();
+        final int[] i1 = {0};
+        final int[] i2 = {size - 1};
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (i1[0] < i2[0]) {
+                    arr5[i1[0]] = (float) (arr5[i1[0]] * Math.sin(0.2f + i1[0] / 5) * Math.cos(0.2f + i1[0] / 5) * Math.cos(0.4f + i1[0] / 2));
+                    i1[0]++;
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (i2[0] > i1[0]) {
+                    arr5[i2[0]] = (float) (arr5[i2[0]] * Math.sin(0.2f + i2[0] / 5) * Math.cos(0.2f + i2[0] / 5) * Math.cos(0.4f + i2[0] / 2));
+                    i2[0]--;
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return System.currentTimeMillis() - a;
+    }
+
 
     static boolean compare(float[] arr1, float[] arr2) {
         int l = arr1.length;
@@ -147,7 +184,8 @@ public class HomeWork5 {
         long t2 = method2();
         long t3 = method3();
         long t4 = method4();
-        if (compare(arr2, arr1) & compare(arr2, arr3) && compare(arr2, arr4))
-            System.out.printf("Two threads full parallel: %dms\nTwo threads by quarters(1+4,2+3): %dms\nTwo threads by half(1+1): %dms\nOne thread: %dms", t4, t3, t2, t1);
+        long t5 = method5();
+        if (compare(arr1, arr2) && compare(arr1, arr3) && compare(arr1, arr4) && compare(arr1, arr5))
+            System.out.printf("Two threads full parallel(counter convergence): %dms\nTwo threads full parallel(even\\odd): %dms\nTwo threads by quarters(1+4\\2+3): %dms\nTwo threads by half(1+1): %dms\nOne thread: %dms", t5, t4, t3, t2, t1);
     }
 }
